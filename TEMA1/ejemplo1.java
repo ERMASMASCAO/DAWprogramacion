0791ad7ce80ff5ac842cb7404a8e811375f251ee
Daniel <danielcaminasadm@gmail.com>
2021-09-27 09:09:55 +0200

    Acabados los dos primeros ejemplos del tema 1

---
+++ TEMA1/ejemplo1.java
@@ -0,0 +1,9 @@
+public class ejemplo1 {
+
+	public static void main(String[] args) {
+		double dividendo = 20.0;
+		double divisor = 6.0;
+		double sumarAlFinal = 3.0;
+		System.out.println((dividendo/divisor) + sumarAlFinal);
+		}
+}
---
+++ TEMA1/ejemplo2.java
@@ -0,0 +1,9 @@
+public class ejemplo2 {
+
+	public static void main(String[] args) {
+		int dividendo = 20;
+		int divisor = 6;
+		int sumarAlFinal = 3;
+		System.out.println((dividendo/divisor) + sumarAlFinal);
+		}
+}
