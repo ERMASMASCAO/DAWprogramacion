04e3c99194a5f4ccfc96ad310aafb9f16008cb7f
Daniel <alupri0338@ieselcaminas.org>
2021-10-04 08:59:35 +0200

    Ejercico 5 terminado

--- Tema1/Ejemplo5.java
+++ Tema1/Ejemplo5.java
@@ -1,6 +1,27 @@
 import java.util.Scanner;
 public class Ejemplo5 {
-    public static void main (String argv[]) {
-        int num1;
-        int num2;
-        
+public static void main(String[] args) {
+
+    //Declara tus variables
+    Scanner lector = new Scanner(System.in);
+    int x,y;
+    int sum, sub, mult, div;
+
+    //Inicio del programa
+    System.out.println("Enter number 1");
+    x = lector.nextInt();
+    System.out.println("Enter number 2");
+    y = lector.nextInt();
+
+    sum = x+y;
+    sub = x-y;
+    mult = x*y;
+    div = x/y;
+
+    System.out.println("sum: " + sum);
+    System.out.println("sub: " + sub);
+    System.out.println("mult: " + mult);
+    System.out.println("div: " + div);
+
+    }
+}
