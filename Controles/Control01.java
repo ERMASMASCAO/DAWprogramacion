2d0d7db8dd41a9dfd397cc968f8ec9db84fa7fc6
Daniel <alupri0338@ieselcaminas.org>
2021-10-13 09:30:01 +0200

    Ejemplo16 en proceso, Control01 entrgado y WhileFor terminado

---
+++ Controles/Control01.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Control01 {
+
+    //El programa debe pedir un número entero de la siguiente forma:
+    public static void main (String argv[]) {
+        float side;
+		float squareArea;
+        System.out.println("Dime el número de lados del polígono:");
+
+    //Si el número introducido es 3 o 4 el programa continua, en caso contrario termina.
+        float x = 3,4;
+        Scanner lector = new Scanner(System.in);
+        if
+    //A continuación el programa debe pedir la longitud del lado del polígono (con decimales): 
+        squareArea = side * side;
+		System.out.println("Introduce la longitud del lado del polígono");
---
+++ Tema1/Ejemplo16.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ejemplo16 {
+
+    //DECLARACIÓN DE VARIABLES
+    public static void main(String argv[]){
+        Scanner lector = new Scanner(System.in);
+        int = x,y;
+        numero = lector.nextInt();
+
+        System.out.println("Introduce un numero entero:");
+    
+    //EMPIEZA EL PROGRAMA
+    lector.close();
+    
