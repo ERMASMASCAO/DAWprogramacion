92a1295bd278ffb18eb8c10fe8bbb57c8b2e53a5
Daniel <alupri0338@ieselcaminas.org>
2021-10-15 09:26:44 +0200

    Ejercicio 17 terminado y corregido sin errores

---
+++ Tema1/Ejemplo17.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Ejemplo17 {
+    
+    
+    
+    public static void main(String [] args ) {
+
+        //DECLARACIÓN DE VARIABLES
+        Scanner lector = new Scanner(System.in);
+        int mes;
+        mes = lector.nextInt();
+
+        //EMPIEZA EL PROGRAMA
+        lector.close();
+        
+        switch (mes){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 10:
+            case 12:
+                System.out.println("Este mes tiene 31 días  :  ");
+                break;
+            case 2:
+                System.out.println("Este mes tiene 28 días  :  ");
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("Este mes tiene 30 días  :  ");
+                break;
+
+        }
+        
+    }
+        
+}
