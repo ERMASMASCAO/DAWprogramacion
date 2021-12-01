499e421322d70ef6d2f53a94ff989021167e14d9
Daniel Prieto <alupri0338@ieselcaminas.org>
2021-12-01 10:30:33 +0100

    01/12/2021 a las 10:30

--- Tema3/Array/Iguales.java
+++ Tema3/Array/Iguales.java
@@ -3,13 +3,15 @@ package Tema3.Array;
 public class Iguales {
     
     public static void main(String[] args) {
-        int [] primero = {1,2,3,4,5,6,7};
+        int [] primero = {1,2,7,4};
         int [] segundo = {1,2,3,4};
         boolean iguales = true;
 
-        if (primero == segundo){
+        if (primero.length != segundo.length){
             System.out.println("Son iguales");
+            
         }else{
+
             for (int i = 0; i < segundo.length; i++) {
                 if (primero[i] != segundo[i]) {
                     iguales = false;
---
+++ Tema3/ParesImpares.java
@@ -0,0 +1,17 @@
+package Tema3;
+
+public class ParesImpares {
+    
+    public static void main(String[] args) {
+        int [] numeros = {2,4,6,7};
+        int ContadorPares;
+        int ContadorImpares;
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros [i] !=2) {
+            }else{
+                (numeros [i] == 0);
+            }
+            
+        }
+    }
+}
