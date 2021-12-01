da4252e483eca073acb30a68d41e575b439889f0
Daniel Prieto <alupri0338@ieselcaminas.org>
2021-12-01 08:49:43 +0100

    Comunes y Utilidades de array hechos

---
+++ Tema3/Array/Comunes.java
@@ -0,0 +1,38 @@
+package Tema3.Array;
+import java.util.Arrays;
+import Tema3.Array.Utilidades;
+/*
+ * Escribir un programa Java para encontrar los elementos comunes entre dos arrays de enteros.
+ */
+public class Comunes {
+	public static String comunes(int[] original, int[] comparar) {
+		String comunes = "";
+		// Recorremos el original para buscar el elemento iésimo
+		for (int i = 0; i < original.length; i++) {
+			// Y ahora buscamos en el comparar el elemento iésimo
+			for (int j = 0; j < comparar.length; j++) {
+				if (original[i] == comparar[j]) {
+					comunes += original[i] + " ";
+					break;
+				}
+			}
+		}
+		return comunes;
+	}
+
+	public static void main(String[] args) {
+		int[] array1 = new int[10];
+		int[] array2 = new int[10];
+
+		Utilidades.rellenaArray(array1, -10, 10);
+		Utilidades.rellenaArray(array2, -10, 10);
+
+		// Antes de empezar a trabajar con el array lo imprimimos
+		System.out.println("Array : " + Arrays.toString(array1));
+		System.out.println("Array : " + Arrays.toString(array2));
+
+		System.out.println("Los elementos comunes son: " + comunes(array1, array2));
+
+	}
+
+}
