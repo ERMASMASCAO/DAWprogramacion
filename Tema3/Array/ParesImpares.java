68cc5e560049a9277cc2ccedaac3ca02026341ec
Daniel Prieto <alupri0338@ieselcaminas.org>
2021-12-03 08:59:21 +0100

    ParesImpares terminado

--- Tema3/Array/ParesImpares.java
+++ Tema3/Array/ParesImpares.java
@@ -4,14 +4,14 @@ public class ParesImpares {
     
     public static void main(String[] args) {
         int [] numeros = {2,4,6,7};
-        int ContadorPares;
-        int ContadorImpares;
+        int contadorPares = 0;
+        int contadorImpares = 0;
         for (int i = 0; i < numeros.length; i++) {
             if (numeros [i] !=2) {
+                contadorImpares++;
             }else{
-                (numeros [i] == 0);
+                contadorPares++;
             }
-            
-        }
+        }System.out.println( "Hay" + contadorImpares + "impares" + "y" + contadorPares + "pares");
     }
 }
