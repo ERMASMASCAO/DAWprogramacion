e5ff10a60868ba957f667d8ff0db12adcf2bbaaf
Daniel Prieto <alupri0338@ieselcaminas.org>
2021-12-15 10:41:38 +0100

    terminados

--- Tema3/Strings/Concat.java
+++ Tema3/Strings/Concat.java
@@ -7,20 +7,20 @@ package Tema3.Strings;
 
 public class Concat {
     
-    public static String concatenar (String cadena1, String cadena2,String cadena3) {
+    public static String concatenar (String cadena1, String cadena2) {
 
         /* Ahora estamos concatenando 3 arrays y hay dos maneras diferentes de hacerlas:
         return cadena1.concat(cadena2).concat(cadena3);
         String primeraConcatenacion =  cadena1.concat(cadena2);
-        return primeraConcatenacion.concat(cadena3);
+        return primeraConcatenacion;
         */
-        return cadena1.concat(cadena2).concat(cadena3);
+        return cadena1.concat(cadena2);
     }
         public static void main(String[] args) {
             String cadena1 = Utilidades.leerCadena("Introduce cadena1");
 		    String cadena2 = Utilidades.leerCadena("Introduce cadena2");
-            String cadena3 = Utilidades.leerCadena("Introduce cadena3");
             System.out.println();
-            System.out.println(concatenar(cadena1, cadena2, cadena3));
+            String resultado = concatenar(cadena1, cadena2);
+            System.out.println(resultado);
         }
 }
--- Tema3/Strings/Contains.java
+++ Tema3/Strings/Contains.java
@@ -6,7 +6,21 @@ package Tema3.Strings;
 */
 
 public class Contains {
-    public static int comparar (String cadena1, String cadena2) {
-        return cadena1.compareToIgnore(cadena2);
+    public static boolean comparar (String cadena1, String cadena2) {
+        return cadena1.contains(cadena2);
+
+        }
+        public static void main(String[] args) {
+            "abcdefg".contains("cd");
+            "abcdefg".contains("cdz");
+            
+            String cadena1 = Utilidades.leerCadena("Introduce cadena 1");
+		    String cadena2 = Utilidades.leerCadena("Introduce cadena 2");
+
+            if (cadena1.contains(cadena2)) {
+                System.out.println("la cadena 1 contiene otra cadena");
+            } else {
+                System.out.println("La cadena 1 no contiene ninguna otra cadena");
+            }
         }
 }
