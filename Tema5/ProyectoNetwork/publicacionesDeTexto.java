eda5b7dc88c84380886cb666b5213b371b433bbe
Daniel Prieto <alupri0338@ieselcaminas.org>
2022-02-16 13:05:43 +0100

    empezamos

--- Tema5/ProyectoNetwork/publicacionesDeTexto.java
+++ Tema5/ProyectoNetwork/publicacionesDeTexto.java
@@ -1,10 +1,21 @@
 package ProyectoNetwork;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class publicacionesDeTexto {
-    
-    private String textoMensaje;
-    private String nombreAutor;
+
+    //Detalles que queremos almacenar:
+
+    private String nomUsuAutor;
+    private String txtMensage;
     private String instPublicacion;
-    private int MeGustas;
-    private String comentariosDeUsuarios;
+    private int meGustas = 0; //Sontador
+    private ArrayList<String> comentariosDeUsuarios;
+
+    //Constructor
+    public publicacionesDeTexto(){
+
+        System.out.println("Usuario: " + nomUsuAutor + " " + "Mensaje: " + " " + txtMensage + " " + "Instantanea: " + " "+ instPublicacion + "MG: " + meGustas + " " + " Comentarios: " + comentariosDeUsuarios);
+    }
 }
--- Tema5/ProyectoNetwork/publicacionesFotograficas.java
+++ Tema5/ProyectoNetwork/publicacionesFotograficas.java
@@ -1,7 +1,20 @@
 package ProyectoNetwork;
 
+import java.util.ArrayList;
+
 public class publicacionesFotograficas {
+
+     //Detalles que queremos almacenar:
     
-    private String titulo;
-    private String imagen;
+    private String textoMensaje;
+    private String nombreAutor;
+    private String instPublicacion;
+    private int meGustas = 0; // Contador
+    private ArrayList<String> comentariosDeUsuarios;
+
+    //Constructor
+    public publicacionesFotograficas(){
+
+        
+    }
 }
