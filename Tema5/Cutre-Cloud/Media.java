7c69f7e6b059fd94c1c1fabacb78ffaf79ff55bf
Daniel Prieto <alupri0338@ieselcaminas.org>
2022-02-23 09:50:14 +0100

    avanzando

--- Tema5/Cutre-Cloud/Media.java
+++ Tema5/Cutre-Cloud/Media.java
@@ -8,12 +8,52 @@ public class Media {
     private String tipo; //audio video o imagen
     private String usuario;
 
-    public void Media (Integer id, String nombre, String contenido, String tipo, String usuario){
-        this.id = id;
+    private static int idGenerator = 0;
+
+    public Media (Integer id, String nombre, String contenido, String tipo, String usuario){
+        this.id = idGenerator++;
         this.nombre = nombre;
         this.contenido = contenido;
         this.tipo = tipo;
         this.usuario = usuario;
     }
-    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
 }
--- Tema5/Cutre-Cloud/Usuario.java
+++ Tema5/Cutre-Cloud/Usuario.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Usuario {
     
     //Atributos de la clase:
@@ -6,10 +8,35 @@ public class Usuario {
     private String email;
     private String password;
 
-    public void Usuario (Integer id, String email, String password){
-        this.id = id;
+    //Lista de usuarios
+    public static ArrayList<Usuario> list = new ArrayList<>();
+    private static int idGenerator = 0;
+    
+    //Constructor
+    public Usuario (String email, String password){
+        this.id = idGenerator++;
         this.email = email;
         this.password = password;
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
 }
