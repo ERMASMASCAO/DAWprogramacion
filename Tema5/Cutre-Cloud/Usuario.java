a1b664ae79ec5e31809096d2b5fa4a94f35e4c1e
Daniel Prieto <alupri0338@ieselcaminas.org>
2022-02-23 09:00:50 +0100

    Cutre-Could

---
+++ Tema5/Cutre-Cloud/Usuario.java
@@ -0,0 +1,15 @@
+public class Usuario {
+    
+    //Atributos de la clase:
+
+    private Integer id;
+    private String email;
+    private String password;
+
+    public void Usuario (Integer id, String email, String password){
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+}
