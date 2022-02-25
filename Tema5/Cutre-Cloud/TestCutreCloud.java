8c5c73963df38fd17a7c533b40d79f65fecdf0fa
Daniel Prieto <alupri0338@ieselcaminas.org>
2022-02-25 08:30:31 +0100

    MediaType creado e implementado a media + cambios diversos.

---
+++ Tema5/Cutre-Cloud/TestCutreCloud.java
@@ -0,0 +1,12 @@
+public class TestCutreCloud {
+    
+    public static void main(String[] args) {
+        Usuario u1 = new Usuario("danirot@cutrecloud.com", "123");
+        Usuario u2 = new Usuario("juanratkid@cutrecloud.com", "456");
+        Usuario u3 = new Usuario("juanratkid@cutrecloud.com", "456");
+        Usuario u4 = new Usuario("raul@cutreclou.com", "sdlfkj");
+
+        
+        System.out.println(u1.getEmail());
+    }
+}   
