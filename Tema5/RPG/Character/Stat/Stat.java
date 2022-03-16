5271bcb4cacd4de60a4f982c69af1346e1e59a2f
Daniel Prieto <alupri0338@ieselcaminas.org>
2022-03-16 09:16:40 +0100

    RPG empezado y algunas modificaciones en stats.

---
+++ Tema5/RPG/Character/Stat/Stat.java
@@ -0,0 +1,27 @@
+public abstract class Stat {
+    
+    //Atribute
+    int value = 0;
+
+    //Constructor
+
+    public  Stat (int value){
+        this.value = value;
+    }
+    //Devuelve el valor actual de las características
+    public int getValue(){
+        return value;
+    }
+    //Incrementa en 1 el valor de las características
+    public void increase(){
+        value++;
+    }
+    //Decrementa en 1 el valor de las características
+    public void decremented(){
+        value--;
+    }
+    //Devuelve el nombre simple de la clase
+    public String toString(){
+       return getClass().getName();
+    }
+}
