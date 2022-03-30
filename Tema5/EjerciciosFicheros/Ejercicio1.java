1282eebe052ff40e71e5473a525a26a6fe923893
Daniel Prieto <alupri0338@ieselcaminas.org>
2022-03-30 12:47:03 +0200

    Ejercicio 1 hecho

---
+++ Tema5/EjerciciosFicheros/Ejercicio1.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Ejercicio1{
+    public static void main(String[] args) {
+        
+        //Abre el directorio padre '/'
+        Scanner opcionesUsuario = new Scanner (System.in);
+        File f = new File("/");
+        System.out.println("---------------------------------------------------");
+        System.out.println("Lista de ficheros y directorios del directorio : /");
+        System.out.println("---------------------------------------------------");
+
+        //Imprimir directorio padre
+        System.out.println("0.- Directorio padre");
+
+        // Imprimir lista de ficheros empezando desde el 1
+        int enumerador = 1;
+        for (File e : f.listFiles()) {
+            enumerador++;
+            if (e.isFile()) {
+                System.out.println(enumerador +".-" + " " + e.length());
+            } else {
+                System.out.println(enumerador +".-" + " " + e + "    <Directorio>");
+            }
+        }
+        String opcion = "";
+        switch (opcion = opcionesUsuario.next()) {
+            case "-1":
+                f = new File("");
+                break;
+            case "0":
+                f = File.listRoots()[0];
+                break; 
+            default: 
+                f = File.listRoots()[0].listFiles()[Integer.parseInt(opcion)];
+                break;
+        }
+        System.out.println("Intorduce -1 para salir");
+        opcionesUsuario.close();
+    }
+}
