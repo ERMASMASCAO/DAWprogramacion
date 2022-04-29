dd2f2a5042a0d1f060ee9ee307c78f4b468e80ad
Daniel Prieto <alupri0338@ieselcaminas.org>
2022-04-29 09:08:38 +0200

    muchas cosas

---
+++ Tema5/Ejemplos ficheros/ejemplos/EscribePrimos.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.io.ByteArrayInputStream;
+
+public class EscribePrimos {
+    
+    public static void main(String[] args) throws IOException {
+        
+       
+}
